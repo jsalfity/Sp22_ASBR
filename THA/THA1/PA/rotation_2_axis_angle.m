9aedd4e78941ead5fe6d0b8219bc23115b0c7b83
jsalfity <j.salfity@utexas.edu>
2022-02-17 16:58:39 -0600

    updated test

--- THA/THA1/PA/rotation_2_axis_angle.m
+++ THA/THA1/PA/rotation_2_axis_angle.m
@@ -10,13 +10,18 @@ function [theta, omega] = rotation_2_axis_angle(R)
         omega = NaN;
       
     elseif sum(diag(R)) == -1
-        theta = pi
+        theta = pi;
         omega = (1/sqrt(2*(1+R(3,3)))) * [R(1,2); R(2,3); 1+R(3,3)];
 
     else
         theta = acos(0.5 * (sum(diag(R)) - 1 ));
-        omega = (R - R') / (2 * sin(theta));
-    
+        omega_hat = (R - R') / (2 * sin(theta));
+        omega_1 = omega_hat(3,2);
+        omega_2 = omega_hat(1,3);
+        omega_3 = omega_hat(2,1);
+        
+        omega = [omega_1; omega_2; omega_3];
+       
     end
  
     return 
