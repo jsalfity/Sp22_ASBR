e7aa265a3ec8f3181db4c0e3aafb554960451ab6
jsalfity <j.salfity@utexas.edu>
2022-02-17 16:34:17 -0600

    euler angles added, check quadrant

--- THA/THA1/PA/rotation_2_ZYZ.m
+++ THA/THA1/PA/rotation_2_ZYZ.m
@@ -6,5 +6,8 @@ function [phi, theta, psi] = rotation_2_ZYZ(R)
     % return: psi 
     % reference: ASBR W3L1 Pg4
 
-    
+   phi = atan2(R(2,3), R(1,3));
+   theta = atan2(sqrt(R(1,3)^2 + R(2,3)^2), R(3,3));
+   psi = atan2(R(3,2), -R(3,1));
+
 end
--- THA/THA1/PA/rotation_2_rpy.m
+++ THA/THA1/PA/rotation_2_rpy.m
@@ -1,10 +1,13 @@
 % PA 1c
-function phi, theta, psi = rotation_2_rpy(R)
+function [phi, theta, psi] = rotation_2_rpy(R)
     % param: R (3x3 rotation matrix)
     % return: phi
     % return: theta
     % return: psi 
     % reference: ASBR W3L1 Pg6
 
-    
+    phi = atan2(R(2,1), R(1,1));
+    theta = atan2(-R(3,1), sqrt(R(3,2)^2 + R(3,3)));
+    psi = atan2(R(3,3), R(3,3));
+
 end
