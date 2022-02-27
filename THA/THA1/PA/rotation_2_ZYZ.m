fca212f276e239cd0715bf66a12d326688f88731
jsalfity <j.salfity@utexas.edu>
2022-02-27 14:39:54 -0600

    screw axis programming begun but not finished

--- THA/THA1/PA/rotation_2_ZYZ.m
+++ THA/THA1/PA/rotation_2_ZYZ.m
@@ -1,13 +1,22 @@
 % PA 1c
-function [phi, theta, psi] = rotation_2_ZYZ(R)
+function [phi, theta, psi] = rotation_2_ZYZ(R, theta_in)
     % param: R (3x3 rotation matrix)
     % return: phi
     % return: theta
     % return: psi 
     % reference: ASBR W3L1 Pg4
 
-   phi = atan2(R(2,3), R(1,3));
-   theta = atan2(sqrt(R(1,3)^2 + R(2,3)^2), R(3,3));
-   psi = atan2(R(3,2), -R(3,1));
+    % check for range of theta_in
+   if theta_in >= 0
+        phi = atan2(R(2,3), R(1,3));
+        theta = atan2(sqrt(R(1,3)^2 + R(2,3)^2), R(3,3));
+        psi = atan2(R(3,2), -R(3,1));
+
+   else
+       phi = atan2(-R(2,3), -R(1,3));
+       theta = atan2(-sqrt(R(1,3)^2 + R(2,3)^2), R(3,3));
+       psi = atan2(-R(3,2), R(3,1));
+      
+   end
 
 end
--- THA/THA1/PA/rotation_2_axis_angle.m
+++ THA/THA1/PA/rotation_2_axis_angle.m
@@ -2,7 +2,7 @@
 function [theta, omega] = rotation_2_axis_angle(R)
     % param: R (3x3 rotation matrix)
     % return: omega, theta (axis-angle representation)
-    % reference: ASBR notes, W2L2, slide 9
+    % reference: ASBR notes, W2L2, slide 9. MR Section 3.2
 
 
     if R == eye(3)
@@ -11,11 +11,16 @@ function [theta, omega] = rotation_2_axis_angle(R)
       
     elseif sum(diag(R)) == -1
         theta = pi;
-        omega = (1/sqrt(2*(1+R(3,3)))) * [R(1,2); R(2,3); 1+R(3,3)];
-
+        if R(3,3) ~= -1
+            omega = (1/sqrt(2*(1+R(3,3)))) * [R(1,3); R(2,3); 1+R(3,3)];
+        elseif R(2,2) ~= -1
+            omega = (1/sqrt(2*(1+R(2,2)))) * [R(1,2); 1+R(2,2); R(3,2)];
+        else %R(1,1) ~= 0
+            omega = (1/sqrt(2*(1+R(1,1)))) * [1+R(1,1); R(2,1); 1+R(3,1)];
+        end
     else
         theta = acos(0.5 * (sum(diag(R)) - 1 ));
-        omega_hat = (R - R') / (2 * sin(theta));
+        omega_hat = (R - R') / (2 * sin(theta)); % screw symmetric matrix
         omega_1 = omega_hat(3,2);
         omega_2 = omega_hat(1,3);
         omega_3 = omega_hat(2,1);
