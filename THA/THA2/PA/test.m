940e84626176e693a4b09d077bd3645a4de59438
jsalfity <j.salfity@utexas.edu>
2022-03-30 14:15:49 -0500

    FK_Space and test added

---
+++ THA/THA2/PA/test.m
@@ -0,0 +1,24 @@
+clc; clear all; close all;
+set(0, 'DefaultFigureColor', 'w')
+
+% Panda Robot struct containing relevant kinematic info
+panda.n_joints = 7;
+panda.M = [1 0 0 0.088; 0 -1 0 0;0 0 -1 0.926;0 0 0 1];
+panda.screw_axes(:,:,1) = [0 -1 0 0;1 0 0 0;0 0 0 0;0 0 0 0];
+panda.screw_axes(:,:,2) = [0 0 1 -0.333;0 0 0 0;-1 0 0 0;0 0 0 0];
+panda.screw_axes(:,:,3) = [0 -1 0 0;1 0 0 0;0 0 0 0;0 0 0 0];
+panda.screw_axes(:,:,4) = [0 0 -1 0.649;0 0 0 0;1 0 0 -0.088;0 0 0 0];
+panda.screw_axes(:,:,5) = [0 -1 0 0;1 0 0 0;0 0 0 0;0 0 0 0];
+panda.screw_axes(:,:,6) = [0 0 -1 1.033;0 0 0 0;1 0 0 0;0 0 0 0];
+panda.screw_axes(:,:,7) = [0 1 0 0;-1 0 0 0.088;0 0 0 0;0 0 0 0];
+panda.qs(:,:,1) = [0 0 0.333];
+panda.qs(:,:,2) = [0 0 0.333];
+panda.qs(:,:,3) = [0 0 0.649];
+panda.qs(:,:,4) = [0.088 0 0.649];
+panda.qs(:,:,5) = [0 0 1.033];
+panda.qs(:,:,6) = [0 0 1.033];
+panda.qs(:,:,7) = [0.088 0 1.033];
+
+% FK_space.m
+input_thetas = [0, 0, 0, 0, 0, 0, 0];
+FK_space(panda, input_thetas);
