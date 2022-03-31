9ee3a09163bb20625e8be77fc1e8dfbb860f2f7f
jsalfity <j.salfity@utexas.edu>
2022-03-31 14:19:49 -0500

    condition, volume, isotropy added

---
+++ THA/THA2/PA/J_condition.m
@@ -0,0 +1,17 @@
+function [mu2] = J_condition(A)
+%J_condition Calculates the condition number, a single scalar mesasure, 
+% defining the sensitivity of the matrix A to scaling errors
+%   param: A (3x3 matrix, usually calculated like:
+%                         Js = J_space(robot, input_thetas);
+%                         A = Js(1:3,:)*Js(1:3,:)';
+%   return: mu2 (float) condition measure
+
+%   reference: MR 5.4
+
+    % calculate e-vecs and e-vals
+    evals = eig(A);
+    eval_min = min(evals);
+    eval_max = max(evals);
+
+    mu1 = eval_max / eval_min;
+end
---
+++ THA/THA2/PA/J_isotropy.m
@@ -0,0 +1,17 @@
+function [mu1] = J_isotropy(A)
+%J_isotropy Calculates the isotropy, a single scalar mesasure, defining how
+%easily the robot can move at a given posture
+%   param: A (3x3 matrix, usually calculated like:
+%                         Js = J_space(robot, input_thetas);
+%                         A = Js(1:3,:)*Js(1:3,:)';
+%   return: mu1 (float) isotropy measure
+
+%   reference: MR 5.4
+
+    % calculate e-vecs and e-vals
+    evals = eig(A);
+    eval_min = min(evals);
+    eval_max = max(evals);
+
+    mu1 = sqrt(eval_max / eval_min);
+end
