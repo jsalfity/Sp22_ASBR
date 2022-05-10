4e4d885daebfe9e6c3507fa1f3d6a893c3ff06db
jsalfity <j.salfity@utexas.edu>
2022-05-10 10:44:57 -0500

    THA4 PA 1, still not working

--- THA/THA4/PA/tool_tip_near_point.m
+++ THA/THA4/PA/tool_tip_near_point.m
@@ -1,5 +1,7 @@
 function [q_curr] = tool_tip_near_point(robot, p_tip, p_goal, q_init, ...    
-                                            q_ub, q_lb, eps, tol);
+        q_ub, q_lb, ...
+        zeta, eta, ...
+        eps, tol);
 %tool_tip_near_point Place robot and new tool tip near point p_gal
 %   param robot: robot configuration
 %   param p_tip: new tool tip
@@ -7,50 +9,72 @@ function [q_curr] = tool_tip_near_point(robot, p_tip, p_goal, q_init, ...
 %   param q_init: initial joint configuration
 %   param q_ub: upper bound for joint limit
 %   param q_lb: lower bound for joint limit
+%   param zeta: weighting for objective function
+%   param eta: weighting for movement
 %   param eps: epsilon for algorithm convergence
 %   param tol: tolerance to keep the tip within
 
 %   return q_curr: joint angles
 
-    % set q_current
-    q_curr = q_init;
+%   reference: ASBR W16 L1-L2
 
-    % iterate through joint angles
-    for i = 1:100
+        % set q_current
+        q_curr = q_init;
+
+        % iterate through joint angles
+        for i = 1:100
 
         % Find tooltip position vector 
-        T_robot = getTransform(robot, q_curr, 'panda_link8');
-%         T_robot = FK_space(robot, q_curr, robot.M, 0);
-
+        T_robot = getTransform(robot, q_curr, 'panda_link8');   %matlab
+        %         T_robot = FK_space(robot, q_curr, robot.M, 0);
+        R = T_robot(1:3, 1:3);
         t = T_robot * p_tip;
         t = t(1:3);
-      
+
         if norm(t - p_goal) < eps
-            % found it !
-            break
+        % found it !
+        break
         end
- 
+
         % space jacobian
-        J = geometricJacobian(robot, q_curr, 'panda_link8');
-%         J = J_space(robot, q_curr);
+        J = geometricJacobian(robot, q_curr, 'panda_link8');    %matlab
+        %         J = J_space(robot, q_curr);
         J_alpha = J(1:3, :);    % rotational
         J_eps = J(4:6, :);      % translation
-        
+
         % Matrices for Optimization
         % cost function
         C = -vec_2_skew_mat(t)*J_alpha + J_eps;
         d = -(t - p_goal); % negative to account for ||Cx - d||
 
-        % constraint
-        % TODO how do we set this up?
-%         A = zeros(robot.n_joints, robot.n_joints);
-%         b = ones(robot.n_joints, 1);
-      
-        % solve lsqr with linear constraints
-        q_delta = lsqr(C, d);
-%         q_delta = lsqlin(C, d, A, b, [], [], q_lb-q_curr, q_ub-q_curr);
+        % no constraints
+        %         q_delta = lsqr(C, d);
+
+        % with constraint
+        % Implement Virtual Fixture through:
+        % Telerobotic Control by Virtual Fixtures for Surgical Applications
+        % equation 22.11
+        A = [];
+        m = 10; %number of vertices for polygon
+        n = 10; % number of vertices for polygon
+        for i=1:m
+        A = [A; [cos(i*2*pi/n)*cos(i*2*pi/m) cos(i*2*pi/n)*sin(i*2*pi/m) sin(i*2*pi/n)]];
+        end
+
+        b = tol * ones(m, 1) - A*d;
+
+        %         C = [sqrt(zeta)*C; sqrt(eta)*-vec_2_skew_mat(R(:,3))*J(1:3, :)];
+        %         d = [sqrt(zeta)*d; sqrt(eta)*zeros(3, 1)];
+
+        q_delta = lsqlin(C, d, A, b, [], [], q_lb-q_curr, q_ub-q_curr);
+
+        % i'm not sure whats causing this error??
+        if size(q_delta, 1) ~= 9
+        continue
+        end
+
         q_curr = q_curr + q_delta;
-        
-    end
+
+        end
 
 end
