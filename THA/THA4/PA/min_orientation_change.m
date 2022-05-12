494922ceffd468f449f7722d249580eb3a984249
Bryant <zhouyujing815@hotmail.com>
2022-05-12 16:18:22 -0500

    added while loop

--- THA/THA4/PA/min_orientation_change.m
+++ THA/THA4/PA/min_orientation_change.m
@@ -14,58 +14,55 @@ function [q_delta] = min_orientation_change(p_tip, p_goal, q_init, ...
 
 %   reference: ASBR W16 L1-L2
 
-% set q_current
-q_curr = q_init;
+error = 1;
 
-% Find tooltip position vector
-% T_robot = getTransform(robot, q_curr, 'panda_link8');   %matlab
+robot = loadrobot('frankaEmikaPanda', 'DataFormat','column');
 run('make_panda.m')
-T_robot = FK_space(panda, q_curr,  panda.M, 1);
-R = T_robot(1:3, 1:3);
-t = T_robot * p_tip;
-t = t(1:3);
 
-% space jacobian
-% J = geometricJacobian(robot, q_curr, 'panda_link8');
-J = J_space(panda, q_curr);
-
-J_alpha = J(1:3, :);    % rotational
-J_eps = J(4:6, :);      % translation
-
-% Matrices for Optimization
-% cost function
-C1 = -vec_2_skew_mat(t)*J_alpha + J_eps; % closest to the goal point
-C2 = -vec_2_skew_mat(R*[0;0;1])*J_alpha; % least orientation change
-d = (t - p_goal); % negative to account for ||Cx - d||
-
-% no constraints
-%         q_delta = lsqr(C, d);
-
-% with constraint
-% Implement Virtual Fixture through:
-% Telerobotic Control by Virtual Fixtures for Surgical Applications
-% equation 22.11
 A = [];
-m = 10; %number of vertices for polygon
-n = 10; % number of vertices for polygon
+m = 100; %number of vertices for polygon
+n = 100; % number of vertices for polygon
 for i=1:m
-    A = [A; [cos(i*2*pi/n)*cos(i*2*pi/m) cos(i*2*pi/n)*sin(i*2*pi/m) sin(i*2*pi/n)]];
+    A = [A; [cos(i*2*pi/n)*cos(i*2*pi/m) cos(i*2*pi/n)*sin(i*2*pi/m) sin(i*2*pi/n) 0 0 0]];
 end
 
-% d = [d;0;0;0];
+while error >= tol
+    figure(2)
+%     plot3(p_goal(1),p_goal(2),p_goal(3),'rx','LineWidth',8)
+    show(robot, [q_init;0;0])
+%     hold on
+    T_robot = FK_space(panda, q_init,  panda.M, 0);
+    R = T_robot(1:3, 1:3);
+    t = T_robot * p_tip;
+    t = t(1:3);
+    
+    J = J_space(panda, q_init);
 
-b = tol * ones(m, 1) - A*d;
+    J_alpha = J(1:3, :);    % rotational
+    J_eps = J(4:6, :);      % translation
+    
+    C1 = -vec_2_skew_mat(t)*J_alpha + J_eps;
+    C2 = -vec_2_skew_mat(R*[0;0;1])*J_alpha;
+    d = (t - p_goal); % negative to account for ||Cx - d||
 
-%         C = [sqrt(zeta)*C; sqrt(eta)*-vec_2_skew_mat(R(:,3))*J(1:3, :)];
-%         d = [sqrt(zeta)*d; sqrt(eta)*zeros(3, 1)];
+    d = [d;0;0;0];
 
-q_delta = lsqlin(w_p*C1+w_a*C2, -w_p*d, A*C1, b, [], [], q_lb-q_curr, q_ub-q_curr);
-
-% i'm not sure whats causing this error??
-% if size(q_delta, 1) ~= 9
-%     continue
-% end
-
-q_delta;
-
+    b = tol * ones(m, 1) - A*d;
+    
+    options = optimoptions('lsqlin','Algorithm','active-set','MaxIterations',2000,'TolCon',1e-20);
+    q_delta = lsqlin(w_p*C1+w_a*C2, -w_p*d(1:3), A*J, b, [], [], q_lb-q_init, q_ub-q_init,zeros(7,1),options);
+    
+    q_init = q_init+q_delta;
+    T_robot = FK_space(panda, q_init,  panda.M, 0);
+    T_tip = T_robot * p_tip;
+    error = norm(T_tip(1:3)-p_goal);
+    i = i+1;
+    if i>=10000
+        break
+    end
+%     show(panda_matlab, [q_init;0;0])
+%     hold on
+    pause(0.01)
+end
+q_curr = q_init;
 end
--- THA/THA4/PA/tool_tip_near_point.m
+++ THA/THA4/PA/tool_tip_near_point.m
@@ -1,4 +1,4 @@
-function [q_delta] = tool_tip_near_point(p_tip, p_goal, q_init, ...
+function [q_curr] = tool_tip_near_point(p_tip, p_goal, q_init, ...
     q_ub, q_lb, tol)
 % tool_tip_near_point Place robot and new tool tip near point p_gal
 %   param p_tip: new tool tip
@@ -12,57 +12,54 @@ function [q_delta] = tool_tip_near_point(p_tip, p_goal, q_init, ...
 
 %   reference: ASBR W16 L1-L2
 
-% set q_current
-q_curr = q_init;
+error = 1;
 
-% Find tooltip position vector
-% T_robot = getTransform(robot, q_curr, 'panda_link8');   %matlab
+robot = loadrobot('frankaEmikaPanda', 'DataFormat','column');
 run('make_panda.m')
-T_robot = FK_space(panda, q_curr,  panda.M, 1);
-R = T_robot(1:3, 1:3);
-t = T_robot * p_tip;
-t = t(1:3);
 
-% space jacobian
-% J = geometricJacobian(robot, q_curr, 'panda_link8');
-J = J_space(panda, q_curr);
-
-J_alpha = J(1:3, :);    % rotational
-J_eps = J(4:6, :);      % translation
-
-% Matrices for Optimization
-% cost function
-C = -vec_2_skew_mat(t)*J_alpha + J_eps;
-d = (t - p_goal); % negative to account for ||Cx - d||
-
-% no constraints
-%         q_delta = lsqr(C, d);
-
-% with constraint
-% Implement Virtual Fixture through:
-% Telerobotic Control by Virtual Fixtures for Surgical Applications
-% equation 22.11
 A = [];
-m = 10; %number of vertices for polygon
-n = 10; % number of vertices for polygon
+m = 100; %number of vertices for polygon
+n = 100; % number of vertices for polygon
 for i=1:m
-    A = [A; [cos(i*2*pi/n)*cos(i*2*pi/m) cos(i*2*pi/n)*sin(i*2*pi/m) sin(i*2*pi/n)]];
+    A = [A; [cos(i*2*pi/n)*cos(i*2*pi/m) cos(i*2*pi/n)*sin(i*2*pi/m) sin(i*2*pi/n) 0 0 0]];
 end
 
-% d = [d;0;0;0];
+while error >= tol
+    figure(1)
+%     plot3(p_goal(1),p_goal(2),p_goal(3),'rx','LineWidth',8)
+    show(robot, [q_init;0;0])
+%     hold on
+    T_robot = FK_space(panda, q_init,  panda.M, 0);
+    R = T_robot(1:3, 1:3);
+    t = T_robot * p_tip;
+    t = t(1:3);
+    
+    J = J_space(panda, q_init);
 
-b = tol * ones(m, 1) - A*d;
+    J_alpha = J(1:3, :);    % rotational
+    J_eps = J(4:6, :);      % translation
+    
+    C = -vec_2_skew_mat(t)*J_alpha + J_eps;
+    d = (t - p_goal); % negative to account for ||Cx - d||
 
-%         C = [sqrt(zeta)*C; sqrt(eta)*-vec_2_skew_mat(R(:,3))*J(1:3, :)];
-%         d = [sqrt(zeta)*d; sqrt(eta)*zeros(3, 1)];
+    d = [d;0;0;0];
 
-q_delta = lsqlin(C, -d, A*C, b, [], [], q_lb-q_curr, q_ub-q_curr);
-
-% i'm not sure whats causing this error??
-% if size(q_delta, 1) ~= 9
-%     continue
-% end
-
-q_delta;
-
+    b = tol * ones(m, 1) - A*d;
+    
+    options = optimoptions('lsqlin','Algorithm','active-set','MaxIterations',2000,'TolCon',1e-20);
+    q_delta = lsqlin(C, -d(1:3), A*J, b, [], [], q_lb-q_init, q_ub-q_init,zeros(7,1),options);
+    
+    q_init = q_init+q_delta;
+    T_robot = FK_space(panda, q_init,  panda.M, 0);
+    T_tip = T_robot * p_tip;
+    error = norm(T_tip(1:3)-p_goal);
+    i = i+1;
+    if i>=10000
+        break
+    end
+%     show(panda_matlab, [q_init;0;0])
+%     hold on
+    pause(0.01)
+end
+q_curr = q_init;
 end
